6f0859de0881c43453e0ca094bc4314c051d7f4e
AssBag <35408327+AssBag@users.noreply.github.com>
2018-01-14 17:33:40 +0000

    Update bot.js

--- super-super-heroku-bot-master/bot.js
+++ super-super-heroku-bot-master/bot.js
@@ -1,15 +1,4 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
 // Here we load the config.json file that contains our token and our prefix values. 
 
 const config = require("./config.json");
